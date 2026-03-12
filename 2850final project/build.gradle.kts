c604dbc84dc197efd45a299d151600b4c50fb2da
Charlie-920 <bpsq2386@leeds.ac.uk>
2026-03-12 16:55:05 +0000

    3.12

---
+++ 2850final project/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("io.ktor.plugin") version "2.3.7"
+}
+
+group = "com.goodfood"
+version = "1.0.0"
+
+application {
+    mainClass.set("com.goodfood.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+val ktorVersion = "2.3.7"
+val exposedVersion = "0.46.0"
+val h2Version = "2.2.224"
+val mysqlVersion = "8.0.33"
+val logbackVersion = "1.4.14"
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-thymeleaf-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-sessions-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-jackson-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-html-builder-jvm:$ktorVersion")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+
+    implementation("com.h2database:h2:$h2Version")
+    implementation("mysql:mysql-connector-java:$mysqlVersion")
+
+    implementation("at.favre.lib:bcrypt:0.10.2")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.22")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
